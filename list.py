e033f75b891b5928247dd734ca675dec614b9cbd
parkresek <antowiranto47@gmail.com>
2022-01-01 23:20:19 +0800

    day 21

--- list.py
+++ list.py
@@ -5,19 +5,31 @@ larik = []
 data = int(input("Banyaknya Data : "))
  
 # perulangan menurut jumlah range
-print ('masukan data : ')
+print ('Masukan Data : ')
 for i in range(0, data):
     value = str(input())
 
 # menambah elemen 
     larik.append(value) 
-     
-print(larik)
+print ('-'*50)     
+print('DATA : ',larik)
 
-# mengurutkan sesuai abjad   
+# menghitung banyak data
+print ('-'*50)
+print ('JUMLAH DATA : ',len(larik))
+
+# mengurutkan sesuai abjad
+print ('-'*50)
+print ('ÚRUTAN DATA SESUAI ABJAD : ')
 larik.sort()
 
 # menampilkan  
 for larik in larik:  
    print(larik)
 
+# menambahkan isi ke list
+tambahan_data = int(input('berapa jumlah data yang ingin di tambahkan : '))
+for a in range (tambahan_data) :
+    larik.append(value)
+print ('data = ',larik)
+   
