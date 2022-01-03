e5454eeaea8cf1836da5c73fcc8618049f7ee100
parkresek <antowiranto47@gmail.com>
2022-01-03 21:34:00 +0800

    day23

--- list.py
+++ list.py
@@ -22,14 +22,53 @@ print ('JUMLAH DATA : ',len(larik))
 print ('-'*50)
 print ('ÚRUTAN DATA SESUAI ABJAD : ')
 larik.sort()
+print ('-'*50)
 
 # menampilkan  
 for larik in larik:  
    print(larik)
 
-# menambahkan isi ke list
-tambahan_data = int(input('berapa jumlah data yang ingin di tambahkan : '))
-for a in range (tambahan_data) :
-    larik.append(value)
-print ('data = ',larik)
+#manipulasi
    
+#pilihan manipulasi
+print ('\t Menu')
+print ('\t 1. capitalize')
+print ('\t 2. uppercase')
+print ('\t 3. lowercase')
+print ('\t 4. replace')
+print ('\t 5. exit')
+print ('-'*50)
+pilihan = int(input('pilih sesuai nomor pada menu : '))
+#looping
+while pilihan >5 or pilihan <1:
+        print ('Masukan pilihan yang ada pada menu')
+        pilihan = int(input('pilih sesuai nomor pada menu : '))
+while pilihan !=0:    
+        if pilihan == 1:
+            print (larik.capitalize())
+            pilihan = larik
+                    
+        elif pilihan == 2:
+            print (larik.upper())
+            pilihan = larik
+                        
+        elif pilihan == 3:
+            print (larik.lower())
+            pilihan = larik
+
+        elif pilihan == 4:
+            replace = larik.replace(str(input ('asal kata : ')),str(input ('menjadi : ')))
+            print (replace)
+            pilihan = larik
+
+        else :
+            print ('TERIMAKASIH')
+            break
+        print ('\t Menu')
+        print ('\t 1. capitalize')
+        print ('\t 2. uppercase')
+        print ('\t 3. lowercase')
+        print ('\t 4. replace')
+        print ('\t 5. exit')
+        print ('-'*50)
+        pilihan = int(input('pilih sesuai nomor pada menu : '))
