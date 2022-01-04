d99ea5f5136db9b954905816bb1b8b5ea2fa199d
parkresek <antowiranto47@gmail.com>
2022-01-05 00:06:41 +0800

    day24

---
+++ bmi.py
@@ -0,0 +1,17 @@
+print("APOTIK");
+print("------");
+print("gejala");
+print("1. pusing");
+print("2. mual");
+print("3. batuk");
+print("===========================");
+kode=float(input("masukan gejala yang anda rasakan (1, 2, 3):\n "));
+if kode==1:
+    print("===========================");
+    print("obat anda adalah bodrek di kombinasi dengan sprit");
+elif kode==2:
+    print("===========================");
+    print("obat anda adalah antimo di kombinasi dengan bodrek"); 
+elif kode==3:
+    print("===========================")
+    print("obat anda adalah komik di kombinasi dengan coca-cola")
