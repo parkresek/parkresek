8524d19241a33fbfdd8d712f7de8fbb4ee7e00b0
parkresek <antowiranto47@gmail.com>
2022-01-05 23:15:20 +0800

    day 25

---
+++ bangun_datar.py
@@ -0,0 +1,11 @@
+print ('='*50)
+print ('kalkulator bagun datar'.center(50,' '))
+print ('='*50)
+print ('MENU'.center(50,' '))
+print ('1.lingkaran')
+
+# rumus
+phie = (3.14)
+r= int(input("\t masukkan nilai jari-jari = "))
+luas = (phie*r*r)
+print("\t luas lingkaran =", luas, "cm2")
