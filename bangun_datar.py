60bfcd4be96d51851bd1b3874b1a5161cf62a572
parkresek <antowiranto47@gmail.com>
2022-01-08 22:25:19 +0800

    day28

--- bangun_datar.py
+++ bangun_datar.py
@@ -2,10 +2,19 @@ print ('='*50)
 print ('kalkulator bagun datar'.center(50,' '))
 print ('='*50)
 print ('MENU'.center(50,' '))
-print ('1.lingkaran')
+print ('1. lingkaran')
+print ('2. persegi')
+pilihan = int (input('masukan pilihan anda = '))
 
 # rumus
-phie = (3.14)
-r= int(input("\t masukkan nilai jari-jari = "))
-luas = (phie*r*r)
-print("\t luas lingkaran =", luas, "cm2")
+if pilihan == 1 :
+    phie = (3.14)
+    r= int(input("\t masukkan nilai jari-jari = "))
+    luas = (phie*r*r)
+    print("\t luas lingkaran =", luas, "cm2")
+elif pilihan == 2:
+    s = int(input ("\t masukan panjang sisi = "))
+    luas = s**2
+    print ("\t luas persegi = ",luas)
+else :
+    print ("selesai")
