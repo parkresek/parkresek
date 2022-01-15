9dc5e0acae3b2e275ede09849ecc1dab09ce2a3e
parkresek <antowiranto47@gmail.com>
2022-01-15 23:01:02 +0800

    day 35

--- konversi_bilangan.py
+++ konversi_bilangan.py
@@ -2,14 +2,28 @@ tampil = ''
 cetak =[]
 desimal = int (input('masukan angka desimal : '))
 print ('')
+
+desimal = int (input('masukan angka desimal :'))
+print ('')
 while desimal != 0:
-    hasil = desimal % 2 #di modulus 2 untuk memperoleh sisa
-    cetak.insert(0, str(hasil)) #membalik hasil modulus
-    desimal = desimal//2 #bilangan desimal dibagi 2 dan tanpa menghasilkan angka koma
+    hasil = desimal % 16 
+    if hasil == 10:
+        hasil = 'A'
+    if hasil == 11:
+        hasil = 'B'
+    if hasil == 12:
+        hasil = 'C'
+    if hasil == 13:
+        hasil = 'D'
+    if hasil == 14:
+        hasil = 'E'
+    if hasil == 15:
+        hasil = 'F'
+    cetak.insert(0, str(hasil))
+    desimal = desimal//16  
     if desimal == 0:
-        for i in range (len(cetak)): #perulangan untuk menggabungkan angka angka hasil modulus yang sebelumnya ada di dalam list menjadi string
+        for i in range (len(cetak)):
             tampil +=  cetak[i]
-    print ('hasilnya adalah : ', tampil)
-    
-                
+print ('hasilnya adalah : ',tampil)
+print ('')                
             
