cfb0657a4191ae71feeda219bd1ef01a2e1a21c8
parkresek <antowiranto47@gmail.com>
2022-02-25 23:40:03 +0800

    day 76

--- game.py
+++ game.py
@@ -1,4 +1,8 @@
-import random as rd 
+import random as rd
+
+print ("="*50)
+print ("Pembagian Kelompok".center(50," "))
+print ("="*50)
 # jumlah mahasiswa
 mahasiswa = int(input("jumlah mahasiswa : "))
 
@@ -6,7 +10,7 @@ mahasiswa = int(input("jumlah mahasiswa : "))
 while mahasiswa > 0:
     mhs =[]
     for a in range (mahasiswa):
-        nama = str(input("Nama Mahasiswa : "))
+        nama = str(input("Nama : "))
         mhs.append(nama)
         mahasiswa-=1  
 nma = mhs
@@ -16,8 +20,9 @@ angka = []
 for num in range (0, len(nma)):
     angka.append(num)
 
-#membagi kelompok 
-total = int(input(" Berapa Kelompok : ")) 
+#membagi kelompok
+print ("_"*50)
+total = int(input("Berapa Kelompok : "))
 kel = [] 
 for x in range(total): 
     sub= [] 
@@ -35,13 +40,14 @@ while len(angka) > 0:
     if count == len(kel)-1:  
         count = 0 
     else:  
-        count+=1 
-
+        count+=1
+print ("="*50)
+print("Hasil Pembagian Kelompok".center(50," "))
 #menampilkan hasil
 nm = total-1 
-for kel_sub in kel: 
-    print("\n") 
+for kel_sub in kel:
+    print("_"*50) 
     print(f"Kelompok {total-nm}") 
     for name in kel_sub: 
-        print(f"{name}") 
+        print(f"-{name}") 
     nm = nm - 1 
